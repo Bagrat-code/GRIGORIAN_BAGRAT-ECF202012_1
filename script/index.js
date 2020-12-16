593982259bc30e220f0d71aa6a5e8a73fd310b2e
Bagrat-code <bagrat.grigorian@laposte.net>
2020-12-16 11:29:50 +0100

    PUSH

---
+++ script/index.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('h4', {delay: 800,});
+ScrollReveal().reveal('h1', {delay: 500,});
+
+ScrollReveal().reveal('.item-1', {delay: 500,});
+ScrollReveal().reveal('.item-2', {delay: 1000,});
+ScrollReveal().reveal('.item-3', {delay: 1500,});
